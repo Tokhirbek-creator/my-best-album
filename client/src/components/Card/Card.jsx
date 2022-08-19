a71bcdf17bb254daebcb2eb65d29d29ab47ef952
Tokhir <karamatov.tohirbek@gmail.com>
2022-08-19 03:44:53 +0300

    Add button for sort photos by date

--- client/src/components/Card/Card.jsx
+++ client/src/components/Card/Card.jsx
@@ -1,13 +1,23 @@
-import React, { memo } from 'react';
+import React, { memo, useState } from 'react';
 import './card.scss';
 import Photo from './Photo/Photo';
 
 function Card({ photos, setPhotos }) {
+  const [value, setValue] = useState(false);
+  const buttonClick = () => {
+    const sorted = photos.reverse();
+    setValue(!value);
+    if (value) {
+      setPhotos(sorted);
+    }
+  };
+
   return (
     <section className="featured section container" id="featured">
       <h2 className="section__title">
         Photos
       </h2>
+      <button type="button" onClick={buttonClick} className="button-33">Sort by date</button>
 
       <div className="featured__container grid">
         {photos.map((photo) => (
